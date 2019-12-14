7e48d846336135a25c7a66178953a1b66abd2fea
nabekou <nabekou0729@gmail.com>
2019-12-15 02:31:15 +0900

    webpack等の準備

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ["airbnb-base", "plugin:prettier/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": "error"
+  }
+};
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# slack_custome_emoji_manager
+# 🚀 Welcome to your new awesome project!
+
+This project has been created using **webpack scaffold**, you can now run
+
+```
+npm run build
+```
+
+or
+
+```
+yarn build
+```
+
+to bundle your application
