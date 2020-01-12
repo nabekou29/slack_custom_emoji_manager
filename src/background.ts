914a054c79b4c0ac04a6ebe1a780e63a3df0b972
nabekou <nabekou0729@gmail.com>
2020-01-13 05:02:01 +0900

    prettier適応

--- src/background.ts
+++ src/background.ts
@@ -1,8 +1,6 @@
 chrome.webRequest.onBeforeSendHeaders.addListener(
   details => {
-    const requestHeaders = details.requestHeaders?.filter(
-      h => h.name !== 'User-Agent'
-    );
+    const requestHeaders = details.requestHeaders?.filter(h => h.name !== 'User-Agent');
     return { requestHeaders };
   },
   { urls: ['<all_urls>'] },
